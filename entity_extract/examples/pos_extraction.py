be2b30b7fce8b2eaf9afa33880597b10d85249a9
RaymondKlass <raymond@klassphoto.com>
2015-02-15 18:28:48 -0500

    further arrange pos extraction class

--- entity_extract/examples/pos_extraction.py
+++ entity_extract/examples/pos_extraction.py
@@ -2,11 +2,7 @@
 from extractor import PosExtractor
 
 p = PosExtractor()
-split = p.sentSplit.split('This is a sentence about the pie in the sky.  If would be interesting.  If only there was')
-print(split)
-tokens = (p.tokenizer.tokenize(s) for s in split)
-print tokens
-pos = (p.pos_tagger.tag(t) for t in tokens)
-print pos
-for data in pos:
-    print data
+data = p.extract_entities('This is a sentence about the pie in the sky.  If would be interesting.  If only there was')
+
+for d in data:
+    print d
--- entity_extract/extractor/pos_extractor.py
+++ entity_extract/extractor/pos_extractor.py
@@ -7,6 +7,19 @@ from .pos_tagger import PosTagger
 class PosExtractor(object):
 
     def __init__(self, debug = False):
+        
+        # These will liekly be run as separate services - but for 
+        # development ease I'm including them here for the time being.
+        
         self.sentSplit = SentSplit()
         self.tokenizer = Tokenizer()
         self.pos_tagger = PosTagger()
+        
+    
+    def extract_entities(self, raw_corpus):
+        
+        sents = self.sentSplit.split(raw_corpus)
+        for sent in sents:
+            tokens = self.tokenizer.tokenize(sent)
+            yield self.pos_tagger.tag(tokens)
+        
