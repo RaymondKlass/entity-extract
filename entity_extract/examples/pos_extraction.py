131d68d7934860f2e2435de22a339c9701951d4e
RaymondKlass <raymond@klassphoto.com>
2015-02-15 17:48:09 -0500

    add tokenizer

--- entity_extract/examples/pos_extraction.py
+++
@@ -1,5 +0,0 @@
-
-from extractor import PosExtractor
-
-p = PosExtractor()
-print( p.sentSplit.split('This is a sentence about the pie in the sky.  If would be interesting.  If only there was'))
--- entity_extract/extractor/pos_extractor.py
+++ entity_extract/extractor/pos_extractor.py
@@ -1,8 +1,10 @@
 # Part of Speach (POS) Based Entity Extraction
 # Aimed at Extracting Relationships
 from .sent_split import SentSplit
+from .tokenizer import Tokenizer
 
 class PosExtractor(object):
 
     def __init__(self, debug = False):
         self.sentSplit = SentSplit()
+        self.tokenizer = Tokenizer()
