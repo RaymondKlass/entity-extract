f6714a3bd9e3a80c4f1c7e3cc9120feeaf66fee8
RaymondKlass <raymond@klassphoto.com>
2015-02-15 17:16:32 -0500

    add examples folder arhcitecture

--- entity_extract/extractor/pos_extractor.py
+++ entity_extract/extractor/pos_extractor.py
@@ -1,7 +1,8 @@
 # Part of Speach (POS) Based Entity Extraction
 # Aimed at Extracting Relationships
+from .sent_split import SentSplit
 
 Class PosExtractor(object):
 
     def __init__(self, debug = False):
-        
+        self.sentSplit = SentSplit()
