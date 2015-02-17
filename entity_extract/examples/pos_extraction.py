ad204472c8e27fd13a1a627ef6b8b88a634f4e81
RaymondKlass <raymond@klassphoto.com>
2015-02-16 19:36:09 -0500

    rearrange dependencies

--- entity_extract/examples/pos_extraction.py
+++ entity_extract/examples/pos_extraction.py
@@ -1,5 +1,6 @@
 
-from entity_extract.extractor import PosExtractor
+#from entity_extract.extractor.extractors import PosExtractor
+from entity_extract.extractor.utilities import SentSplit
 
 p = PosExtractor()
 data = p.extract_entities('This is a sentence about the pie in the sky.  If would be interesting.  If only there was')
--- entity_extract/extractor/__init__.py
+++
@@ -1,1 +0,0 @@
-from pos_extractor import PosExtractor
