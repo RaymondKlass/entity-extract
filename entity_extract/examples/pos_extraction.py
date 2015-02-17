8cc8edc7d3020a96ab70cd32ade146a9a9f245c4
RaymondKlass <raymond@klassphoto.com>
2015-02-16 19:21:03 -0500

    modify import to accomodate package installation python path vars available

--- entity_extract/examples/pos_extraction.py
+++ entity_extract/examples/pos_extraction.py
@@ -1,5 +1,5 @@
 
-from extractor import PosExtractor
+from entity_extract.extractor import PosExtractor
 
 p = PosExtractor()
 data = p.extract_entities('This is a sentence about the pie in the sky.  If would be interesting.  If only there was')
