0461a6051a0a4381112cb72e4a29a3d7a69e5a57
RaymondKlass <raymond@klassphoto.com>
2015-02-17 19:20:19 -0500

    update example

--- entity_extract/examples/pos_extraction.py
+++ entity_extract/examples/pos_extraction.py
@@ -4,6 +4,8 @@ from entity_extract.extractor.utilities import SentSplit, Tokenizer
 from entity_extract.extractor.extractors import PosExtractor
 from entity_extract.extractor.pos_tagger import PosTagger
 
+
+
 #p = PosExtractor()
 #data = p.extract_entities('This is a sentence about the pie in the sky.  If would be interesting.  If only there was')
 
