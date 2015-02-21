ee168fa6787f7de4999e1e41b8eb4cda482df143
RaymondKlass <raymond@klassphoto.com>
2015-02-21 11:03:38 -0500

    update chunker

--- entity_extract/examples/pos_extraction.py
+++ entity_extract/examples/pos_extraction.py
@@ -3,15 +3,18 @@
 from entity_extract.extractor.utilities import SentSplit, Tokenizer
 from entity_extract.extractor.extractors import PosExtractor
 from entity_extract.extractor.pos_tagger import PosTagger
+from entity_extract.extractor.parsers import ChunkParser
 
 # Initialize Services
 sentSplitter = SentSplit()
 tokenizer = Tokenizer()
 tagger = PosTagger()
+chunker = ChunkParser()
 
 
 sents = sentSplitter.split('This is a sentence about the pie in the sky.  If would be interesting.  If only there was')
 for sent in sents:
     tokens = tokenizer.tokenize(sent)
     tags = tagger.tag(tokens)
-    print tags
+    chunks = chunker.parse(tags)
+    print chunks
--- entity_extract/extractor/parsers/chunk_parser.py
+++ entity_extract/extractor/parsers/chunk_parser.py
@@ -3,11 +3,11 @@ from nltk.corpus import conll2000
 from nltk.chunk.util import conlltags2tree
 
 
-Class ChunkParser(object):
+class ChunkParser(object):
     
     def __init__(self, chunk_types=['NP']):
         train_sents = conll2000.chunked_sents('train.txt', chunk_types)
-		self.ChunkParser = ChunkParser(train_sents)
+		self.ChunkParser = nltk.ChunkParser(train_sents)
     
     def parse(self, tagged_sent):
         return self.ChunkParser.parse(tagged_sent)		
