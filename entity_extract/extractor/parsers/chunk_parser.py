a70cf4d8e02ddd4249e04e6707d2501629c74db0
RaymondKlass <raymond@klassphoto.com>
2015-02-21 10:55:38 -0500

    add chunk parser

---
+++ entity_extract/extractor/parsers/chunk_parser.py
@@ -0,0 +1,13 @@
+import nltk
+from nltk.corpus import conll2000
+from nltk.chunk.util import conlltags2tree
+
+
+Class ChunkParser(object):
+    
+    def __init__(self, chunk_types=['NP']):
+        train_sents = conll2000.chunked_sents('train.txt', chunk_types)
+		self.ChunkParser = ChunkParser(train_sents)
+    
+    def parse(self, tagged_sent):
+        return self.ChunkParser.parse(tagged_sent)		
