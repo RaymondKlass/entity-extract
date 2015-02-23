b430fda6ad4ef6946ca8878edba81ac32c74a868
RaymondKlass <raymond@klassphoto.com>
2015-02-23 16:42:35 -0500

    re-arrange comments

--- entity_extract/extractor/extractors/pos_extractor.py
+++ entity_extract/extractor/extractors/pos_extractor.py
@@ -1,6 +1,24 @@
 # Part of Speach (POS) Based Entity Extraction
 # Aimed at Extracting Relationships
 
+# Use regex matching to identify relational phrases
+# VP?
+# V(W*P)?
+# 
+# V1: 'RB'
+# V2: 'MD', 'VB', 'VBD', 'VBP', 'VBZ', 'VBG', 'VBN'
+# V3: 'RP', 'RB'
+# (V1)? + (V2) + (V3)?
+#
+# P1: 'RB'
+# P2: 'IN', 'TO', 'RP'
+# P3: 'RB'
+# (P1)? + (P2) + (P3)?
+#
+# W: 'PRP$', 'CD', 'DT', 'JJ', 'JJS', 'JJR', 'NN', 'NNS', 'NNP', 'NNPS', 'POS', 'RB', 'RBR', 'RBS', 'VBN', 'VBG'
+# (W1)
+#
+
 class PosExtractor(object):
 
     def __init__(self, debug = False):
@@ -8,23 +26,7 @@ class PosExtractor(object):
         
     # Generator to return tokenized sentences
     def extract_entities(self, token_sent):
-        # Use regex matching to identify relational phrases
-        # VP?
-        # V(W*P)?
-        # 
-        # V1: 'RB'
-        # V2: 'MD', 'VB', 'VBD', 'VBP', 'VBZ', 'VBG', 'VBN'
-        # V3: 'RP', 'RB'
-        # (V1)? + (V2) + (V3)?
-        #
-        # P1: 'RB'
-        # P2: 'IN', 'TO', 'RP'
-        # P3: 'RB'
-        # (P1)? + (P2) + (P3)?
-        #
-        # W: 'PRP$', 'CD', 'DT', 'JJ', 'JJS', 'JJR', 'NN', 'NNS', 'NNP', 'NNPS', 'POS', 'RB', 'RBR', 'RBS', 'VBN', 'VBG'
-        # (W1)
-        #
+
         
         
         
