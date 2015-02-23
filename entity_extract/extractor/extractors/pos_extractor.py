e1f3b656e0fed4ecce6638e83ec1e3f07ec78ba9
RaymondKlass <raymond@klassphoto.com>
2015-02-23 16:37:26 -0500

    update extractor with basic pos pattern

--- entity_extract/extractor/extractors/pos_extractor.py
+++ entity_extract/extractor/extractors/pos_extractor.py
@@ -8,4 +8,25 @@ class PosExtractor(object):
         
     # Generator to return tokenized sentences
     def extract_entities(self, token_sent):
+        # Use regex matching to identify relational phrases
+        # VP?
+        # V(W*P)?
+        # 
+        # V1: 'RB'
+        # V2: 'MD', 'VB', 'VBD', 'VBP', 'VBZ', 'VBG', 'VBN'
+        # V3: 'RP', 'RB'
+        # (V1)? + (V2) + (V3)?
+        #
+        # P1: 'RB'
+        # P2: 'IN', 'TO', 'RP'
+        # P3: 'RB'
+        # (P1)? + (P2) + (P3)?
+        #
+        # W: 'PRP$', 'CD', 'DT', 'JJ', 'JJS', 'JJR', 'NN', 'NNS', 'NNP', 'NNPS', 'POS', 'RB', 'RBR', 'RBS', 'VBN', 'VBG'
+        # (W1)
+        #
+        
+        
+        
+        
         return True
