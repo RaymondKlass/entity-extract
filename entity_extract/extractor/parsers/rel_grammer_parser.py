f269f669e77ed9faef8550a43cc8db13d5220f67
RaymondKlass <raymond@klassphoto.com>
2015-02-27 16:21:24 -0500

    define grammer parser in own module instead of with extractor

--- entity_extract/extractor/parsers/rel_grammer_parser.py
+++ entity_extract/extractor/parsers/rel_grammer_parser.py
@@ -1,4 +1,21 @@
 import nltk
 
 class RelationGrammerParser(object):
-    pass
+    
+    def __init__(self):
+        grammer = r"""
+            V: {<RB>?<MD|VB|VBD|VBP|VBG|VBN><RP|RB>?}
+            P: {<RB>?<IN|TO|RP><RB>?}
+            W: {<PRP$|CD|DT|JJ|JJS|JJR|NN|NNS|NNP|NNPS|POS|RB|RBR|RBS|VBN|VBG>*}
+            RelP1: {(<V><P>?)*}
+            RelP2: {(<V>(<W>*<P>)?)*}
+            RelPhrase: {(<RelP1>*|<RelP2>*)?}
+        """
+        
+        self.parser = nltk.RegexpParser(grammer)
+    
+    
+    def parse(self, tokenized_sent):
+        
+        return self.parser.parse(tokenized_sent)
+    
