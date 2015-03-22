e78ab91526499cba4ff237b77e7f8ecfb71d8677
RaymondKlass <raymond@klassphoto.com>
2015-03-22 18:40:36 -0400

    parse boundaries test progress

--- entity_extract/tests/test_parsers.py
+++ entity_extract/tests/test_parsers.py
@@ -16,6 +16,10 @@ class base_tree_parser(unittest.TestCase):
         """ Test boundary parse method to see that phrase boundaries are properly
             recorded. 
         """
+        
+        # We need to create a tree with which to test our parseBoundaries method
+        
+        
         mock_parse.return_value = 'Hello'
         print self.base_tree.parse('A new sentence')    
         self.assertTrue(True)
