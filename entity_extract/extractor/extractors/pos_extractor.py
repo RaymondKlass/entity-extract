629b307764c718274f9dd0288a5dba38d4e9a2c7
RaymondKlass <raymond@klassphoto.com>
2015-03-30 21:44:36 -0400

    update relation extractor wording and comments

--- entity_extract/extractor/extractors/pos_extractor.py
+++ entity_extract/extractor/extractors/pos_extractor.py
@@ -6,6 +6,13 @@ class PosExtractor(object):
         pass
         
     
-    def extract_entities(self, token_sent, np_bounds, rel_bounds):
+    def extract_relationships(self, token_sent, np_bounds, rel_bounds):
+        """ Extract relationships based upon entity's found in Noun Phrases and 
+            Relational Phrases - A simple approach - if we find Relational Phrases
+            that are surrounded by Noun Phrases, then we'll call this a relationship
+        """
+        
         pass
 
+
+    
